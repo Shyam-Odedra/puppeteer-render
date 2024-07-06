cb52c8dba19e5dfa77aeeb9c867f9b4abf5c2bfb
Shyam <shyamodedra929@gmail.com>
2024-07-06 12:22:47 +0530

    Updated Scraping

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -16,30 +16,29 @@ const scrapeLogic = async (res) => {
   });
   try {
     const page = await browser.newPage();
-
-    await page.goto("https://developer.chrome.com/");
+    // Navigate the page to a URL
+    await page.goto('https://developer.chrome.com/');
 
     // Set screen size
     await page.setViewport({ width: 1080, height: 1024 });
 
     // Type into search box
-    await page.type(".search-box__input", "automate beyond recorder");
+    await page.type('.devsite-search-field', 'automate beyond recorder');
 
     // Wait and click on first result
-    const searchResultSelector = ".search-box__link";
+    const searchResultSelector = '.devsite-result-item-link';
     await page.waitForSelector(searchResultSelector);
     await page.click(searchResultSelector);
 
     // Locate the full title with a unique string
     const textSelector = await page.waitForSelector(
-      "text/Customize and automate"
+      'text/Customize and automate'
     );
-    const fullTitle = await textSelector.evaluate((el) => el.textContent);
+    const fullTitle = await textSelector?.evaluate(el => el.textContent);
 
     // Print the full title
-    const logStatement = `The title of this blog post is ${fullTitle}`;
-    console.log(logStatement);
-    res.send(logStatement);
+    console.log('The title of this blog post is "%s".', fullTitle);
+    res.send(`Title of Blog post ==> ${fullTitle}`);
   } catch (e) {
     console.error(e);
     res.send(`Something went wrong while running Puppeteer: ${e}`);
